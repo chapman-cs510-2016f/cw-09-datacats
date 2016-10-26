cff1f2764bc789a4bb5cbcce2f6fd4cefe75702a
simrath ratra <ratra101@mail.chapman.edu>
2016-10-26 04:25:37 +0000

    classwork

--- src/factorial.c
+++ src/factorial.c
@@ -7,14 +7,20 @@ int factorial(int n) {
     /* By convention in C, all variables must by type defined at the top
      * of each function */
     int fac = n;
-    
-    while (n > 0) {
+    int count = n;
+    if (n == 0) {
+    fac = 1; 
+    }
+    if (n < 0) {
+    fac = 0;
+    }
+    while (count > 1) {
         /* var-- reads the contents of an integer variable, then decrements
            --var decrements the contents of an integer variable, then returns
          */
-        n--;
+        count--;
         // a *= b is equivalent to a = a * b
-        fac *= n;
+        fac *=count;
     }
     return fac;
 }
